aec9dadd683183f4d5bdbec848ef4cfd86e3b858
0xlukem <lucas.malizia27@gmail.com>
2026-07-24 20:55:08 -0300

    test 01 phyton

---
+++ demo/widget.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function UserList({ fetchUsers, onSelect }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers().then(setUsers);
+  }, []);
+
+  return (
+    <ul>
+      {users.map((u, i) => (
+        <li key={i} onClick={() => onSelect(u)}>
+          {u.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
